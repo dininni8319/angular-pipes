828b9655d67252adf0f2321f1da241133669f0d3
Salvatore Dininni <s.dininni@yahoo.com>
2022-05-16 17:24:38 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+
+  onNameChange(value: KeyboardEvent) {
+    const target = value.target as HTMLTextAreaElement;
+     this.name = target.value
+  }
 }
