ecf9f26139630df6881650c3e085880909b1c6f4
Salvatore Dininni <s.dininni@yahoo.com>
2022-05-16 19:10:11 +0200

    pipes project implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   name: string = '';
+  date: string = '';
+  amount: undefined |number;
+  height: undefined | number;
 
+  car = { 
+    make: 'Toyota',
+    model: 'Camry',
+    year: 1934
+  }
+  
   onNameChange(value: KeyboardEvent) {
     const target = value.target as HTMLTextAreaElement;
      this.name = target.value
+  }
+
+  onDateChange(value: KeyboardEvent) {
+    const target = value.target as HTMLTextAreaElement;
+    this.date = target.value;
+  }
+
+  onAmountChange(e: KeyboardEvent) {
+    const target = e.target as HTMLTextAreaElement;
+    this.amount = parseFloat(target.value);
+    // console.log(this.amount, 'testing the amount');
+  }
+
+  onHeightChange(value: KeyboardEvent) {
+    const target = value.target as HTMLTextAreaElement;
+    this.height = parseFloat(target.value);
+
   }
 }
